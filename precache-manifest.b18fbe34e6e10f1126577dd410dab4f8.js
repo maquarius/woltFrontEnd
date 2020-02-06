adaee24400e311966af9aa90e9ab7c9982c02575
Marius van der Busse <mariusvanderbusse@hotmail.com>
2020-02-06 20:48:25 +0200

    Updates

---
+++ precache-manifest.b18fbe34e6e10f1126577dd410dab4f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738738474c460e41037c9b9acbe30727",
+    "url": "/woltFrontEnd/index.html"
+  },
+  {
+    "revision": "aef53abe81800150b94e",
+    "url": "/woltFrontEnd/static/css/main.7099718a.chunk.css"
+  },
+  {
+    "revision": "77007729b1579c93da3b",
+    "url": "/woltFrontEnd/static/js/2.c7662007.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/woltFrontEnd/static/js/2.c7662007.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aef53abe81800150b94e",
+    "url": "/woltFrontEnd/static/js/main.29482a90.chunk.js"
+  },
+  {
+    "revision": "68a93ba35c879ac14764",
+    "url": "/woltFrontEnd/static/js/runtime-main.e94b04c6.js"
+  }
+]);
